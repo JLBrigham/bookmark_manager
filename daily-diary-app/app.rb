6c8280f7ef5d5ba5055a09dc8a15b9c498caab22
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-02-25 13:05:22 +0100

    pass first feature test

--- daily-diary-app/app.rb
+++ daily-diary-app/app.rb
@@ -1,9 +1,21 @@
 require 'sinatra/base'
 
 class DailyDiary < Sinatra::Base 
+    enable :sessions
+    set :session_secret, 'super secret'
 
   get '/' do
     erb :index
+  end
+
+  post '/add_diary_entry' do
+    session[:diary_entry] = params[:diary_entry]
+    redirect '/'
+  end
+
+  get '/view_diary' do
+    @diary_entry = session[:diary_entry]
+    erb :view_diary
   end
 
     run! if app_file == $0
